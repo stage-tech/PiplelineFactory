afc04a2385a6a4b32cec4af2bc9ea08c6e7b6a77
Abdo <abdo@axetay.co.uk>
2020-04-28 12:39:21 +0100

    sanitize / properly

--- src/PipeLineTemplate/bin/utility.ts
+++ src/PipeLineTemplate/bin/utility.ts
@@ -1,6 +1,6 @@
 export class Utility {
     public static    sanitizeStackName  (rawName :string ): string {
-        return rawName.replace(new RegExp('_', 'g'), '-').replace(new RegExp('//', 'g'), '-');
+        return rawName.replace(new RegExp('_', 'g'), '-').replace(new RegExp('/', 'g'), '-');
 
     }
 }
