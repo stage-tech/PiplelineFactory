714e8f2ceb291c64106041900c89e45f5f5ed539
Abdo <abdo@axetay.co.uk>
2020-05-11 09:35:35 +0100

    fix lambda function apigw response

--- src/PipeLineFactory/schedulingLambdaSrc/branchMonitor.js
+++ src/PipeLineFactory/schedulingLambdaSrc/branchMonitor.js
@@ -1,52 +1,64 @@
-const { TriggerProject} = require('./triggerBuild')  ;
+const { TriggerProject } = require('./triggerBuild');
 
-function getBranchNamefromRef(refvalue){
+function getBranchNamefromRef(refvalue) {
   return refvalue;
 
 }
 
 
-exports.apiBranchCreated =  function(event) {
-    var payload = event.body;
-    console.debug(payload);
-    var buildParameter = JSON.parse(payload);
-   
-   TriggerProject(buildParameter, "create")
-}
-
-exports.apiBranchDeleted =  function(event) {
+exports.apiBranchCreated = function (event, context, callback) {
   var payload = event.body;
   console.debug(payload);
   var buildParameter = JSON.parse(payload);
- 
- TriggerProject(buildParameter, "destroy")
+
+  var result = TriggerProject(buildParameter, "create")
+  callback(null , ({
+    'statusCode': 200,
+    'headers': { 'Content-Type': 'application/json' },
+    'body': JSON.stringify(result)
+  }));
+
 }
 
-exports.snsBranchDeleted = function(event) {
+exports.apiBranchDeleted = function (event) {
+  var payload = event.body;
+  console.debug(payload);
+  var buildParameter = JSON.parse(payload);
+  
+  var result = TriggerProject(buildParameter, "destroy")
+  callback(null , ({
+    'statusCode': 200,
+    'headers': { 'Content-Type': 'application/json' },
+    'body': JSON.stringify(result)
+  }));
+ 
+}
+
+exports.snsBranchDeleted = function (event) {
   var payload = event.Records[0].Sns.Message;
   console.debug(payload);
   var githubContext = JSON.parse(payload);
-  console.debug(githubContext);  
- 
+  console.debug(githubContext);
+
   var buildParameter = {
-    "repository_name" : githubContext.repository.name,
-    "repository_owner" : githubContext.repository.owner.login,
-    "branch" : getBranchNamefromRef(githubContext.ref)
+    "repository_name": githubContext.repository.name,
+    "repository_owner": githubContext.repository.owner.login,
+    "branch": getBranchNamefromRef(githubContext.ref)
   };
 
   TriggerProject(buildParameter, "destroy")
 }
 
-exports.snsBracnhCreated = function(event) {
+exports.snsBracnhCreated = function (event) {
   var payload = event.Records[0].Sns.Message;
   console.debug(payload);
   var githubContext = JSON.parse(payload);
-  console.debug(githubContext);  
- 
+  console.debug(githubContext);
+
   var buildParameter = {
-    "repository_name" : githubContext.repository.name,
-    "repository_owner" : githubContext.repository.owner.login,
-    "branch" : getBranchNamefromRef(githubContext.ref)
+    "repository_name": githubContext.repository.name,
+    "repository_owner": githubContext.repository.owner.login,
+    "branch": getBranchNamefromRef(githubContext.ref)
   };
 
   TriggerProject(buildParameter, "create")
