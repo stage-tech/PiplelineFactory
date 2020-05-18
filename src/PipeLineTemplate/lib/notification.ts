f9ce6a2b475294d39e3ddf8119cceb9bb196df38
Justas Veršekys <justas.versekys@telesoftas.com>
2020-05-18 09:13:50 +0300

    feat: add slack channel configuration

---
+++ src/PipeLineTemplate/lib/notification.ts
@@ -0,0 +1,29 @@
+import * as cdk from "@aws-cdk/core";
+import * as chatbot from '@aws-cdk/aws-chatbot';
+import * as ssm from '@aws-cdk/aws-ssm';
+import { BuildOperationsDetails } from "./buildOperationsDetails";
+import { IPipeline } from "@aws-cdk/aws-codepipeline";
+
+export class Notification extends cdk.Construct {
+    constructor(scope: cdk.Construct, id: string, props: BuildOperationsDetails, pipeline: IPipeline) {
+        super(scope, id);
+        
+        let slackChannelName = 'sphinx-env-';
+        if(props.githubRepositoryBranch === 'master' || props.githubRepositoryBranch === 'bumastemra-uat'){
+            slackChannelName += props.githubRepositoryBranch;
+        } else {
+            slackChannelName += 'other';
+        }
+
+        let slackChannelId = ssm.StringParameter.valueForSecureStringParameter(scope, slackChannelName, 1);
+
+        new chatbot.CfnSlackChannelConfiguration(this, props.projectName + 'SlackChannelConfiguration', {
+            loggingLevel: 'INFO',
+            configurationName: props.projectName + 'CodePipelineStateChangesConfiguration',
+            iamRoleArn: props.buildAsRoleArn,
+            slackChannelId: slackChannelId,
+            slackWorkspaceId: 'T5J1W20JV',
+            // snsTopicArns: [notificationTopic.topicArn],
+          });
+    }
+}
