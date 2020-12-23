e205611c6cecdd303f9c28e70c26955d38426191
Abdo <abdo@axetay.co.uk>
2020-12-23 19:12:00 +0000

    trigger monitoring every 1 hr

--- src/PipeLineFactory/lib/monitor/monitor.ts
+++ src/PipeLineFactory/lib/monitor/monitor.ts
@@ -3,7 +3,10 @@ import * as sqs from "@aws-cdk/aws-sqs";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as iam from "@aws-cdk/aws-iam";
-import { SqsEventSource } from '@aws-cdk/aws-lambda-event-sources';
+import * as events from '@aws-cdk/aws-events';
+import * as eventTargets from '@aws-cdk/aws-events-targets';
+
+import { SqsEventSource } from "@aws-cdk/aws-lambda-event-sources";
 
 export interface MonitorProps {
   triggerCodeS3Key: string;
@@ -13,15 +16,13 @@ export interface MonitorProps {
 
 export class Monitor extends cdk.Construct {
   public readonly buildProjectArn: string;
-  constructor(
-    scope: cdk.Construct,
-    id: string,
-    props: MonitorProps
-  ) {
+  constructor(scope: cdk.Construct, id: string, props: MonitorProps) {
     super(scope, id);
     const queue = new sqs.Queue(this, "repository_discovery_jobs", {
       queueName: `repository_discovery_jobs`,
     });
+
+    const stackName = cdk.Stack.of(this).stackName;
 
     const sourceCodeBucket = s3.Bucket.fromBucketAttributes(
       this,
@@ -41,30 +42,44 @@ export class Monitor extends cdk.Construct {
     };
 
     const lambdaRole = new iam.Role(this, "Role_LambdaFunction", {
-      roleName: `PLF-${cdk.Stack.of(this).stackName}-Repository-Monitor`,
+      roleName: `PLF-${stackName}-Repository-Monitor`,
       assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
     });
 
-    lambdaRole.addManagedPolicy( iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"))
-    lambdaRole.attachInlinePolicy(new iam.Policy(this , "SecretManagerPolicy" , {
-        statements : [ new iam.PolicyStatement({
-          actions : ['secretsmanager:GetSecretValue'],
-          effect : iam.Effect.ALLOW,
-          resources : ['arn:aws:secretsmanager:*:secret:/pipeline-factory/organization/*']
-        }) ]
-    }))
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+    lambdaRole.attachInlinePolicy(
+      new iam.Policy(this, "SecretManagerPolicy", {
+        statements: [
+          new iam.PolicyStatement({
+            actions: ["secretsmanager:GetSecretValue"],
+            effect: iam.Effect.ALLOW,
+            resources: [
+              "arn:aws:secretsmanager:*:secret:/pipeline-factory/organization/*",
+            ],
+          }),
+        ],
+      })
+    );
 
-    lambdaRole.attachInlinePolicy(new iam.Policy(this , "SqsPolicy" , {
-      statements : [ new iam.PolicyStatement({
-        actions : ['sqs:*'],
-        effect : iam.Effect.ALLOW,
-        resources : [queue.queueArn]
-      }) ]
-  }))
+    lambdaRole.attachInlinePolicy(
+      new iam.Policy(this, "SqsPolicy", {
+        statements: [
+          new iam.PolicyStatement({
+            actions: ["sqs:*"],
+            effect: iam.Effect.ALLOW,
+            resources: [queue.queueArn],
+          }),
+        ],
+      })
+    );
 
-    new lambda.Function(this, "Lambda_Repository_Monitor", {
+    const repositoryMonitor = new lambda.Function(this, "Lambda_Repository_Monitor", {
       runtime: lambda.Runtime.NODEJS_10_X,
-      functionName: `${cdk.Stack.of(this).stackName}-Repository-Monitor`,
+      functionName: `${stackName}-Repository-Monitor`,
       handler: "dist/monitor/handler-monitor-repositories.handler",
       role: lambdaRole,
       code: lambdaCode,
@@ -72,16 +87,26 @@ export class Monitor extends cdk.Construct {
       timeout: cdk.Duration.seconds(10),
     });
 
-    const pipelineManagementHandler =    new lambda.Function(this, "Lambda_Pipeline_Manager", {
-      runtime: lambda.Runtime.NODEJS_10_X,
-      functionName: `${cdk.Stack.of(this).stackName}-Pipeline-Manager`,
-      handler: "dist/monitor/handler-pipeline-management.handler",
-      role: lambdaRole,
-      code: lambdaCode,
-      environment: environmentVariables,
-      timeout: cdk.Duration.seconds(10),
+    new events.Rule(this, "Schedule", {
+      ruleName: `${stackName}-trigger-repository-monitoring`,
+      targets: [new eventTargets.LambdaFunction(repositoryMonitor)],
+      schedule: events.Schedule.rate(cdk.Duration.hours(1)),
     });
 
-    pipelineManagementHandler.addEventSource(new SqsEventSource(queue))
+    const pipelineManagementHandler = new lambda.Function(
+      this,
+      "Lambda_Pipeline_Manager",
+      {
+        runtime: lambda.Runtime.NODEJS_10_X,
+        functionName: `${stackName}-Pipeline-Manager`,
+        handler: "dist/monitor/handler-pipeline-management.handler",
+        role: lambdaRole,
+        code: lambdaCode,
+        environment: environmentVariables,
+        timeout: cdk.Duration.seconds(10),
+      }
+    );
+
+    pipelineManagementHandler.addEventSource(new SqsEventSource(queue));
   }
 }
