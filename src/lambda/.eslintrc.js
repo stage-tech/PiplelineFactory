d06c45492e15d8e021355948fb6cb9b286589e42
benas.milasauskas <benas.milasauskas@telesoftas.com>
2021-06-11 12:49:55 +0300

    fix npmrc added to gitignore

--- src/lambda/.eslintrc.js
+++ src/lambda/.eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
-    // tsconfigRootDir: 'src/lambda',
     project: ['tsconfig.json', 'tsconfig.test.json'],
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
