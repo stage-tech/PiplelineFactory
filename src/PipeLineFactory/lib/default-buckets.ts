36bac48eaf517d887e066cbd22ee2d867b69be5e
igngzl <46370774+igngzl@users.noreply.github.com>
2021-06-21 11:26:40 +0300

    feat: add encryption for pipeline-factory-events sns topic (#32)
    Co-authored-by: Ignas <ignas.guzelis@telesoftas.com>

--- src/PipeLineFactory/lib/default-buckets.ts
+++ src/PipeLineFactory/lib/default-buckets.ts
@@ -1,12 +1,13 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as ssm from "@aws-cdk/aws-ssm";
-import * as kms from "@aws-cdk/aws-kms";
 import * as iam from "@aws-cdk/aws-iam";
+import * as kms from "@aws-cdk/aws-kms";
 
 export interface DefaultBucketsProps {
   existingBucketName?: string;
   buildRole : iam.Role;
+  kmsEncryptionKey: kms.Key;
 }
 
 export default class DefaultBuckets extends cdk.Construct {
@@ -16,21 +17,13 @@ export default class DefaultBuckets extends cdk.Construct {
     super(scope, id);
 
     const stack = cdk.Stack.of(this);
-
-    const bucketEncryptionKey = new kms.Key(this, "BucketEncryption", {
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
-    });
-
-    bucketEncryptionKey.grantEncryptDecrypt(
-      new iam.ArnPrincipal(props.buildRole.roleArn)
-    );
     
     this.transientArtifactsBucket = new s3.Bucket(this, "transientBucket", {
       bucketName: `${stack.stackName.toLowerCase()}-${stack.account}-${
         stack.region
       }-transient`,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      encryptionKey: bucketEncryptionKey,
+      encryptionKey: props.kmsEncryptionKey,
       encryption: s3.BucketEncryption.KMS,
     });
 
@@ -51,7 +44,7 @@ export default class DefaultBuckets extends cdk.Construct {
           stack.region
         }-artifacts`,
         removalPolicy: cdk.RemovalPolicy.RETAIN,
-        encryptionKey: bucketEncryptionKey,
+        encryptionKey: props.kmsEncryptionKey,
         encryption: s3.BucketEncryption.KMS,
       });
     }
